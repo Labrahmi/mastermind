669d8363e87b2a8b755893e3562984d820abca2c
youssef labrahmi <ylabrahm@c2r6p8.1337.ma>
2023-01-14 22:42:50 +0100

    all

--- mastermind.c
+++ mastermind.c
@@ -6,7 +6,7 @@
 /*   By: ylabrahm <ylabrahm@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 15:07:10 by ylabrahm          #+#    #+#             */
-/*   Updated: 2023/01/14 19:46:11 by ylabrahm         ###   ########.fr       */
+/*   Updated: 2023/01/14 22:41:20 by ylabrahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,8 +40,9 @@ typedef struct s_games
 	int		id;
 	int		tryes;
 	int		won;
-	int 	**playes_moves;
-	int		**playes_results;
+	int 	playes_moves[10][4];
+	int		playes_results[10][4];
+	int		winner_numbers[4];
 	struct s_games *next;
 }	t_games;
 
@@ -98,6 +99,24 @@ void	ft_enqueue(t_queue *q, t_games *data)
 	
 }
 
+void	ft_init_array(t_games *var)
+{
+	int i, j;
+
+	i = 0;
+	while (i < 4)
+	{
+		j = 0;
+		while (j < 4)
+		{
+			var->playes_moves[i][j] = -1;
+			var->playes_results[i][j] = -1;
+			j++;
+		}
+		i++;
+	}
+}
+
 t_games	*ft_set_game(int parties)
 {
 	int	array[4];
@@ -111,7 +130,6 @@ t_games	*ft_set_game(int parties)
 	double elapsed_time;
 	// -
 	t_games	*new_game;
-
 	time(&start_time);
 	new_game = (t_games *) malloc(sizeof(t_games));
 	new_game->id = parties;
@@ -119,29 +137,7 @@ t_games	*ft_set_game(int parties)
 	new_game->tryes = 0;
 	new_game->won = 0;
 	new_game->next = NULL;
-	new_game->playes_moves = NULL;
-	new_game->playes_results = NULL;
-
-	
-	new_game->playes_moves = (int **) malloc(sizeof(int *));
-
-	
-
-/*
-	time_t start_time, end_time;
-	double elapsed_time;
-	time(&start_time);
-	sleep(3);
-	usleep(100000);
-	time(&end_time);
-	elapsed_time = difftime(end_time, start_time);
-	printf("Elapsed time is: %f\n", elapsed_time);
-	return 0;
-*/	
-
-
-
-
+	ft_init_array(new_game);
 	srand(time(0));
 	for (i = 0; i < 4; i++)
 	{
@@ -159,14 +155,8 @@ t_games	*ft_set_game(int parties)
 		i = 0;
 		while (i < 4)
 		{
-			printf("K:%d\n", k);
-			printf("i:%d\n", i);
-			printf("user_resp[i]:%d\n", user_resp[i]);
-			new_game->playes_moves[k] = (int *) malloc(sizeof(int));
+			new_game->winner_numbers[i] = array[i];
 			new_game->playes_moves[k][i] = user_resp[i];
-			printf("[%d]\n", new_game->playes_moves[k][i]);
-			sleep(1);
-			printf("-\n");
 			i++;
 		}
 		i = 0;
@@ -185,16 +175,19 @@ t_games	*ft_set_game(int parties)
 			result += check_exist(user_resp[i], array);
 			result += check_pos(user_resp[i], array[i]);
 			if (result)
+			{
+				new_game->playes_results[k][i] = result;
 				printf("\033[1;28m\033[0m\033[1;31m%c\033[0m ", print_res(result));
+			}
 			i++;
 			win += result;
 		}
 		if (win == 8)
 		{
+			printf("\nYou Won The Game!\n");
+			sleep(4);
 			new_game->won = 1;
 			break;
-			// printf("\nYou Win The Game\n");
-			// return 0;
 		}
 		printf("\n");
 		j++;
@@ -203,19 +196,49 @@ t_games	*ft_set_game(int parties)
 	new_game->tryes = (j + 1);
 	if ((j + 1) == 11)
 		new_game->tryes--;
-	// printf("\nHard Luck Next Time!\n");
 	time(&end_time);
 	new_game->time = difftime(end_time, start_time);
 	return (new_game);
 }
 
+int ft_count(t_queue *q)
+{
+	int	i;
+	t_games	*new_game;
 
+	new_game = q->head;
+	i = 0;
+	while (new_game)
+	{
+		new_game = new_game->next;
+		i++;
+	}
+	return (i);
+}
+
+t_games	*ft_get_game(int id, t_queue *q)
+{
+	t_games	*new_game;
+	
+	new_game = q->head;
+	while (new_game)
+	{
+		if (new_game->id == (id - 1))
+			return (new_game);
+		new_game = new_game->next;
+	}
+	return (NULL);
+}
 
 int main(void) 
 {
+	int		i, j;
 	int		parties;
 	int		user_menu_chosen_value;
 	char	*user_name;
+	t_games *new_game;
+	int		c;
+	
 	
 
 	
@@ -232,35 +255,108 @@ int main(void)
 	scanf("%s", user_name);
 	fflush(stdin);
 	system("clear");
-	printf("Welcome To Mastermind Game, %s\n", user_name);
-	printf("please choose an option: \n");
-	choose:
-	// -
-	printf("\n\n");
-	printf("	[1]: Play a new game\n");
-	printf("	[2]: See History\n");
-	printf("	[3]: See Game rules\n");
-	printf("	[4]: Exit\n");
-	printf("\n\n");
-	// -
-	scanf("%d", &user_menu_chosen_value);
-	fflush(stdin);
-	if (!(user_menu_chosen_value == 1 || user_menu_chosen_value == 2 || user_menu_chosen_value == 3 || user_menu_chosen_value == 4))
+	
+	
+	while (1)
 	{
-		fflush(stdout);
-	    system("clear");
-		printf("please choose a valid number!\n");
-		goto choose;
-	}
-	if (user_menu_chosen_value == 1)
-	{
-		ft_enqueue(&new_que, ft_set_game(parties));
-		printf("\nid:%d\ntime:%f\ntryes:%d\nwon:%d\n", new_que.head->id, new_que.head->time, new_que.head->tryes, new_que.head->won);
-		printf("%d ", new_que.head->playes_moves[0][0]);
-		printf("%d ", new_que.head->playes_moves[0][1]);
-		printf("%d ", new_que.head->playes_moves[0][2]);
-		printf("%d ", new_que.head->playes_moves[0][3]);
-		parties++;
+		printf("\n");
+		system("clear");
+		usleep(300000);
+		printf("Welcome To Mastermind Game, %s\n", user_name);
+		printf("please choose an option: \n");
+		choose:
+		// -
+		printf("\n\n");
+		printf("	[1]: Play a new game\n");
+		printf("	[2]: See History\n");
+		printf("	[3]: See Game rules\n");
+		printf("	[4]: Exit\n");
+		printf("\n\n");
+		// -
+		scanf("%d", &user_menu_chosen_value);
+		fflush(stdin);
+		if (!(user_menu_chosen_value == 1 || user_menu_chosen_value == 2 || user_menu_chosen_value == 3 || user_menu_chosen_value == 4))
+		{
+			fflush(stdout);
+			system("clear");
+			printf("please choose a valid number!\n");
+			goto choose;
+		}
+		if (user_menu_chosen_value == 1)
+		{
+			ft_enqueue(&new_que, ft_set_game(parties));
+			parties++;
+		}
+		if (user_menu_chosen_value == 2)
+		{
+			system("clear");
+			int	count_nodes;
+			int	chosen_partie;
+			
+			count_nodes = ft_count(&new_que);
+			chosen_partie = 0;
+
+			printf("You Have (%d) old parties\n", count_nodes);
+			printf("Which one you want to see from (1 to %d): ", count_nodes);
+			scanf("%d", &chosen_partie);
+			fflush(stdin);
+			
+			if (!(chosen_partie >= 1 && chosen_partie <= count_nodes))
+				break;
+			else
+			{
+				new_game = ft_get_game(chosen_partie, &new_que);
+				printf("\n\n");
+				printf("		*) Game n : %d \n", (new_game->id + 1));
+				printf("		*) Time   : %.0fs\n", new_game->time);
+				printf("		*) Tryes  : %d \n", new_game->tryes);
+				if (new_game->won == 1)
+					printf("		*) Won   : Yes \n");
+				else
+					printf("		*) Won   : No \n");
+				
+				printf("		*) Winning Moves : ");
+				i = 0;
+				while (i < 4)
+				{
+					printf("%d ", new_game->winner_numbers[i]);
+					i++;
+				}
+				printf("\n");
+				printf("		*) Player Moves : \n");
+				i = 0;
+				while (i < new_game->tryes)
+				{
+					j = 0;
+					printf("			");
+					while (j < 4)
+					{
+						printf("%d ", new_game->playes_moves[i][j]);
+						j++;
+					}
+					j = 0;
+					printf("	|	");
+					while (j < 4)
+					{
+						printf("\033[1;28m\033[0m\033[1;31m%c\033[0m ", print_res(new_game->playes_results[i][j]));
+						j++;
+					}
+					printf("\n");
+					i++;
+				}
+				printf("\n\n");
+				printf("type [c] to continue : ");
+				scanf("%d", &c);
+				fflush(stdin);
+			}
+		}
+		
+		if (user_menu_chosen_value == 4)
+		{
+			system("clear");
+			printf("GoodBye %s!\n", user_name);
+			exit(0);
+		}
 	}
     return 0;
 }
